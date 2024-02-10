d0b6d959566eef5cbaac1a9cb4dcfff0cbeda806
Ashish Jayamohan <ashishjayamohan@Ashishs-MacBook-Pro-2.local>
2024-02-10 10:11:19 -0800

    add submit button and text field

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/TaskFormFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/TaskFormFragment.java
@@ -16,13 +16,16 @@ public class TaskFormFragment extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.addtaskform, container, false);
-        Button btnClose = view.findViewById(R.id.btnClose);
+
+        Button btnClose = view.findViewById(R.id.close_button);
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss(); // Close the popup when the close button is clicked
             }
         });
+        
+        Button btnSubmit = view.findViewById(R.id.submit_button);
 
         return view;
 
