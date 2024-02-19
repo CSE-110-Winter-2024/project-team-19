692ed0d26c1504a7efe398a33a588fa00f77be56
Abdullah Ahmed <aahmed@outlook.com>
2024-02-18 22:51:08 -0800

    Readded mock ui button

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/TaskListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/TaskListFragment.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.successorator.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.os.Looper;
@@ -10,12 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -23,18 +24,19 @@ import java.util.ArrayList;
 import java.util.List;
 import android.os.Handler;
 import edu.ucsd.cse110.successorator.MainViewModel;
-import edu.ucsd.cse110.successorator.data.db.TaskEntity;
-import edu.ucsd.cse110.successorator.data.db.TasksDao;
 import edu.ucsd.cse110.successorator.databinding.FragmentTaskListBinding;
 import edu.ucsd.cse110.successorator.databinding.ListItemTaskBinding;
-import edu.ucsd.cse110.successorator.lib.domain.Task;
 import edu.ucsd.cse110.successorator.util.DateRolloverMock;
+
 
 /*
 This class was adapted from the CardListFragment class provided in CSE 110 Lab 5.
 https://docs.google.com/document/d/1hpG8UJLVru_pGrT3vCMee2vjA-8HadWwjyk5gGbUatI/edit
  */
 public class TaskListFragment extends Fragment {
+
+    private static final String PREF_TASK_DATE = "task_date";
+    private static final String PREF_TASK_TIME = "task_time";
     private MainViewModel activityModel;
     private FragmentTaskListBinding view;
 
@@ -42,10 +44,18 @@ public class TaskListFragment extends Fragment {
     private TextView DateDisplay;
     private Handler handler;
 
-    private LocalDateTime lastDate;
+    private LocalDateTime lastTime;
+
+    private LocalDate lastDate;
+
+    private LocalTime rolloverTime;
     private ListItemTaskBinding taskItem;
 
     private TaskListAdapter adapter;
+
+    private boolean deleteFlag = false;
+
+    private SharedPreferences sharedPreferences;
 
 
     public TaskListFragment() {
@@ -105,20 +115,8 @@ public class TaskListFragment extends Fragment {
 
 
         DateRolloverMock mockTime = new DateRolloverMock(myDateObj);
-        lastDate = LocalDateTime.now();
-
-        //To start the mock uncomment line below
-        myDateObj = mockTime.getFakeTime();
-
-
-
-
-        LocalTime rolloverTime = LocalTime.of(14, 0);
-        if (myDateObj.toLocalTime().isBefore(rolloverTime)) {
-            myDateObj = myDateObj.with(rolloverTime);
-        } else {
-            myDateObj = myDateObj.plusDays(1).with(rolloverTime);
-        }
+        lastTime = LocalDateTime.now();
+        lastDate = LocalDate.now();
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
 
         String StringOfDate = myDateObj.format(myFormatObj).toString();
@@ -139,7 +137,7 @@ public class TaskListFragment extends Fragment {
 
             @Override
             public void run() {
-                updateCurrentTime(false);
+                updateCurrentDate();
                 handler.postDelayed(this, 1000);
             }
 
@@ -148,13 +146,11 @@ public class TaskListFragment extends Fragment {
         };
         handler.post(updateTimeRunnable);
 
-
-
         view.mockButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //calling updateTime with mocked = true just moves date forward
-                updateCurrentTime(true);
+                updateCurrentTime();
                 //Call deleteCompletedTasks from the taskDao
                 activityModel.deleteCompletedTasks();
 
@@ -167,19 +163,32 @@ public class TaskListFragment extends Fragment {
     }
 
 
-    private void updateCurrentTime(boolean mocked) {
+    private void updateCurrentDate() {
         LocalDateTime timeNow = LocalDateTime.now();
-
-        //check if the timeNow is a day ahead of lastDate OR myDateObj is a day ahead
-        if (timeNow.isAfter(lastDate) || mocked == true)
+        LocalDate dateNow = LocalDate.now();
+        LocalTime tNow = LocalTime.now();
+        if (dateNow.isAfter(lastDate) && tNow.isAfter(LocalTime.of(2, 0))) {
+            lastDate = dateNow;
+            activityModel.deleteCompletedTasks();
+        } else if (timeNow.isAfter(lastTime) )
         {
-            if (mocked == false){lastDate = timeNow;}
-            else {lastDate = lastDate.plusDays(1);}
-
-
+            lastTime = timeNow;
             DateTimeFormatter myFormatObj2 = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
 
-            String StringOfDate2 = lastDate.format(myFormatObj2).toString();
+            String StringOfDate2 = lastTime.format(myFormatObj2).toString();
+            DateDisplay.setText(StringOfDate2);
+
+            //here we need to call some method to remove all tasks that are completed
+
+        }
+    }
+
+    private void updateCurrentTime() {
+        LocalDateTime timeNow = LocalDateTime.now();
+        lastTime = lastTime.plusDays(1);
+            DateTimeFormatter myFormatObj2 = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+
+            String StringOfDate2 = lastTime.format(myFormatObj2).toString();
             DateDisplay.setText(StringOfDate2);
 
             //here we need to call some method to remove all tasks that are completed
@@ -187,4 +196,6 @@ public class TaskListFragment extends Fragment {
         }
 
 
-}}
+
+
+    }
